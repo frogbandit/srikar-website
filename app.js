7cb6df3a2abc1b254b3bfe5f3dfa3f3743694f44
James Xue <jx2218@columbia.edu>
2018-04-08 16:49:51 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app);
+var path = require("path");
+var favicon = require('serve-favicon');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+app.get('/',function(req, res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+
+app.get('/projects',function(req, res){
+  res.sendFile(path.join(__dirname+'/public/projects.html'));
+});
+
+app.get('/test',function(req, res){
+  res.sendFile(path.join(__dirname+'/public/test_page.html'));
+});
+
+
+var listener = server.listen(process.env.PORT || 3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('App listening at http://%s:%s', host, port);
+});
